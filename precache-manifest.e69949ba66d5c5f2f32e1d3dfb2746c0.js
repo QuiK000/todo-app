864f6389af0819065272044012fb6e4b8e4db75f
QuiK <teamquiksteam@gmail.com>
2020-01-19 21:30:26 +0200

    'todo-app'

---
+++ precache-manifest.e69949ba66d5c5f2f32e1d3dfb2746c0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "00df3e0f18dc901939284bff205a93f6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a58df0aa05722ca0e256",
+    "url": "/static/css/main.502377a8.chunk.css"
+  },
+  {
+    "revision": "a65021b0066d3d058d27",
+    "url": "/static/js/2.cfe94f6a.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.cfe94f6a.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a58df0aa05722ca0e256",
+    "url": "/static/js/main.3f86ad33.chunk.js"
+  },
+  {
+    "revision": "f483d1f7cd20d8e3322f",
+    "url": "/static/js/runtime-main.70302f69.js"
+  }
+]);
